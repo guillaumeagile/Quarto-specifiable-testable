b76b578a36cf41650c4d5fd3737c26ea0d6d7e07
guillaume <guillaume.saintetienne@marvin-bot.com>
2023-06-05 18:36:58 +0200

    refacto pour lisibilité

--- src/test/kotlin/specifions/AgileTourToulouse2023Spec.kt
+++ src/test/kotlin/specifions/AgileTourToulouse2023Spec.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import quatro.*
 
-class PremioereBehaviorSpec : BehaviorSpec({
+class AgileTourToulouse2023Spec : BehaviorSpec({
     given("Il y a") {
         var plateau = PlateauQuarto.Build()
         var bouteilleBlanche1 = PieceDeQuarto(hauteur = Hauteur.HAUT,
@@ -15,18 +15,15 @@ class PremioereBehaviorSpec : BehaviorSpec({
         var bouteille3 = bouteille2.differentPour(forme = Forme.BOUCHON)
         var bouteilleDeLadversaire = bouteille3.differentPour(Habillage.UNI)
 
-
-        val plateauAJouer = plateau.placer(bouteilleBlanche1).En(0,0)
-            .placer(bouteille2).En(1,0)
-            .placer(bouteille3).En(2,0)
+        val plateauAJouer = plateau.placer(bouteilleBlanche1).En(x = 0,y = 0)
+            .placer(bouteille2).En(x = 1, y = 0)
+            .placer(bouteille3).En(x = 2, y = 0)
 
         `when`("je fais un truc") {
-           var plateauResultat = plateauAJouer.placer(bouteilleDeLadversaire).En(3,0)
-
+           var plateauResultat = plateauAJouer.placer(bouteilleDeLadversaire).En(x = 3, y = 0)
 
             then("il s'est passé quelque chose") {
                 plateauResultat.cestGagné shouldBe true
-
             }
         }
     }
