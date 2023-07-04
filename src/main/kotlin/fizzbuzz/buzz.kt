42da3d08d6c883f0792e930bc2d933ed931ae67e
guillaume <guillaume.saintetienne@marvin-bot.com>
2023-07-05 00:49:13 +0200

    reformat

--- src/main/kotlin/fizzbuzz/buzz.kt
+++ src/main/kotlin/fizzbuzz/buzz.kt
@@ -3,14 +3,13 @@ package fizzbuzz
 class buzz {
     companion object {
 
-        val rules = mapOf({ x: Int -> (x % 3 == 0) } to "Fizz", { x: Int -> (x % 5 == 0) } to "Buzz", { x: Int -> (x % 7 == 0) } to "Quiz")
+        val rules = mapOf(
+            { x: Int -> (x % 3 == 0) } to "Fizz",
+            { x: Int -> (x % 5 == 0) } to "Buzz",
+            { x: Int -> (x % 7 == 0) } to "Quiz")
 
         fun eval(nombre: Int): String {
-            /* var res = ""
-              for (rule in rules) {
-                   if (rule.key(nombre))
-                       res = res + rule.value
-                    }   */
+
             var res = rules.entries.fold("") { acc, rule ->
                 if (rule.key(nombre)) (acc + rule.value) else acc
             }
